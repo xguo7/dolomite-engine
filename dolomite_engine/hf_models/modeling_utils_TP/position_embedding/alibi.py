ed7e376def0403e6617ea08e7d49205a94747337
Mayank Mishra <mayank31398@gmail.com>
2024-05-17 07:20:48 -0400

    alibi

--- dolomite_engine/hf_models/modeling_utils_TP/position_embedding/alibi.py
+++ dolomite_engine/hf_models/modeling_utils_TP/position_embedding/alibi.py
@@ -10,9 +10,11 @@ from ..TP import get_tensor_parallel_group_manager
 class Alibi_TP(Alibi):
     def __init__(self, num_heads: int) -> None:
         nn.Module.__init__(self)
-
         self.num_heads = num_heads
 
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
         closest_power_of_2 = 2 ** math.floor(math.log2(self.num_heads))
         base = torch.tensor(2 ** (-(2 ** -(math.log2(closest_power_of_2) - 3))), dtype=torch.float32)
         powers = torch.arange(1, 1 + closest_power_of_2, dtype=torch.int32)
@@ -26,7 +28,7 @@ class Alibi_TP(Alibi):
 
         tp_rank = get_tensor_parallel_group_manager().get_rank()
         tp_world_size = get_tensor_parallel_group_manager().get_world_size()
-        num_heads_tp = num_heads // tp_world_size
+        num_heads_tp = self.num_heads // tp_world_size
         slopes = slopes[tp_rank * num_heads_tp : (tp_rank + 1) * num_heads_tp]
 
         self.register_buffer("slopes", slopes, persistent=False)
