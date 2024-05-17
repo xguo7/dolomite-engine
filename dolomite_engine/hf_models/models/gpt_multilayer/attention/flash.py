88325549a37911da2557e6b2c893e9b95af9085f
Mayank Mishra <mayank31398@gmail.com>
2024-05-17 18:45:58 -0400

    flash

--- dolomite_engine/hf_models/models/gpt_multilayer/attention/flash.py
+++ dolomite_engine/hf_models/models/gpt_multilayer/attention/flash.py
@@ -37,47 +37,63 @@ class MultiLayerFlashAttention2(MultiLayerAttention):
 
         batch_size, query_length = query.shape[:2]
 
-        key_length = key.shape[1]
-
-        indices_k, cu_seqlens_k, max_seqlen_k = get_unpad_data(attention_mask)
-
-        key = index_first_axis(
-            key.reshape(batch_size * key_length, self.num_key_value_heads, self.head_dim), indices_k
-        )
-        value = index_first_axis(
-            value.reshape(batch_size * key_length, self.num_key_value_heads, self.head_dim), indices_k
-        )
-
-        if query_length == key_length:
-            query = index_first_axis(query.reshape(batch_size * key_length, self.num_heads, self.head_dim), indices_k)
-            cu_seqlens_q = cu_seqlens_k
-            max_seqlen_q = max_seqlen_k
-            indices_q = indices_k
-        elif query_length == 1:
-            max_seqlen_q = 1
-            cu_seqlens_q = torch.arange(
-                batch_size + 1, dtype=torch.int32, device=query.device
-            )  # There is a memcpy here, that is very bad.
-            indices_q = cu_seqlens_q[:-1]
-            query = query.squeeze(1)
+        if attention_mask is None:
+            attn_output = flash_attention(
+                query,
+                key,
+                value,
+                cu_seqlens_q=None,
+                cu_seqlens_k=None,
+                max_seqlen_q=None,
+                max_seqlen_k=None,
+                dropout_p=dropout_p,
+                softmax_scale=softmax_scale,
+                causal=self.causal,
+            )
         else:
-            # The -q_len: slice assumes left padding.
-            attention_mask = attention_mask[:, -query_length:]
-            query, indices_q, cu_seqlens_q, max_seqlen_q = unpad_input(query, attention_mask)
+            key_length = key.shape[1]
 
-        attn_output = flash_attention(
-            query,
-            key,
-            value,
-            cu_seqlens_q=cu_seqlens_q,
-            cu_seqlens_k=cu_seqlens_k,
-            max_seqlen_q=max_seqlen_q,
-            max_seqlen_k=max_seqlen_k,
-            dropout_p=dropout_p,
-            softmax_scale=softmax_scale,
-            causal=self.causal,
-        )
-        attn_output = pad_input(attn_output, indices_q, batch_size, query_length)
+            indices_k, cu_seqlens_k, max_seqlen_k = get_unpad_data(attention_mask)
+
+            key = index_first_axis(
+                key.reshape(batch_size * key_length, self.num_key_value_heads, self.head_dim), indices_k
+            )
+            value = index_first_axis(
+                value.reshape(batch_size * key_length, self.num_key_value_heads, self.head_dim), indices_k
+            )
+
+            if query_length == key_length:
+                query = index_first_axis(
+                    query.reshape(batch_size * key_length, self.num_heads, self.head_dim), indices_k
+                )
+                cu_seqlens_q = cu_seqlens_k
+                max_seqlen_q = max_seqlen_k
+                indices_q = indices_k
+            elif query_length == 1:
+                max_seqlen_q = 1
+                cu_seqlens_q = torch.arange(
+                    batch_size + 1, dtype=torch.int32, device=query.device
+                )  # There is a memcpy here, that is very bad.
+                indices_q = cu_seqlens_q[:-1]
+                query = query.squeeze(1)
+            else:
+                # The -q_len: slice assumes left padding.
+                attention_mask = attention_mask[:, -query_length:]
+                query, indices_q, cu_seqlens_q, max_seqlen_q = unpad_input(query, attention_mask)
+
+            attn_output = flash_attention(
+                query,
+                key,
+                value,
+                cu_seqlens_q=cu_seqlens_q,
+                cu_seqlens_k=cu_seqlens_k,
+                max_seqlen_q=max_seqlen_q,
+                max_seqlen_k=max_seqlen_k,
+                dropout_p=dropout_p,
+                softmax_scale=softmax_scale,
+                causal=self.causal,
+            )
+            attn_output = pad_input(attn_output, indices_q, batch_size, query_length)
 
         attn_output = attn_output.view(batch_size, query_length, -1)
 
